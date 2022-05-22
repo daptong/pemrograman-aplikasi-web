aed4050bb10f3d11d1bb392b57336c7b63050832
daptong <46106793+daptong@users.noreply.github.com>
2022-05-22 21:41:50 +0700

    fixed AJAX url redirect

--- Project/SISCKA-E/js/helperedit.js
+++ Project/SISCKA-E/js/helperedit.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
-	// code to read selected table row cell data (values).
-	$(".btnSelect").on('click',function(){
-		 var currentRow = $(this).closest("tr");
-		 var col1 = currentRow.find("td:eq(0)").html();
-		 var col2 = currentRow.find("td:eq(1)").html();
-		 var col3 = currentRow.find("#state_laundry").val();
+    $('.btnSelect').on('click', function(){
+        var currentRow = $(this).closest("tr");
+	var col1 = currentRow.find("td:eq(0)").html();
+	var col3 = currentRow.find("#state_laundry").val();
 
         var items = {
             transaksi_id: col1,
@@ -13,12 +11,18 @@ $(document).ready(function(){
 
         $.ajax({
             type: "POST",
-            url: "test-admin-helper-change-transaksi-status.php",
+            url: "admin-helper-change-driver-packet.php",
             data: items,
             success: function(data){
                 alert(data);
-                location.href="test-admin-check-laundry.php";
-            }
+                location.href = "admin-check-laundry.php";
+		window.location.reload();
+            },
+	    error: function(data){
+	    	alert(data);
+		location.href = "admin-check-laundry.php";
+		window.location.reload();
+	    }
         });
-	});
+    });
 });

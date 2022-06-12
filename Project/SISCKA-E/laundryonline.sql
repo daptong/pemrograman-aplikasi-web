a6928f624b24ad8c8d77bf40cf7dc4516e243f88
Muh. Daffa Firmansyah <46106793+daptong@users.noreply.github.com>
2022-06-12 13:39:24 +0700

    updated laundryonline sql #1

--- Project/SISCKA-E/laundryonline.sql
+++ Project/SISCKA-E/laundryonline.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 21, 2022 at 05:15 PM
+-- Generation Time: Jun 12, 2022 at 08:38 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -20,28 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `laundryonline`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `driver`
---
-
-CREATE TABLE `driver` (
-  `driver_id` int(10) NOT NULL,
-  `driver_nama` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `driver`
---
-
-INSERT INTO `driver` (`driver_id`, `driver_nama`) VALUES
-(1, 'Ahmad Naufal'),
-(2, 'Agus Joko Susilo'),
-(3, 'Samuel Anderson'),
-(4, 'Edwin Nurdiansyah'),
-(5, 'Freddy Yohanes Patty');
 
 -- --------------------------------------------------------
 
@@ -68,7 +46,9 @@ CREATE TABLE `paket` (
 INSERT INTO `paket` (`paket_id`, `transaksi_id`, `nama_customer`, `tanggal_pemesanan`, `tanggal_pengiriman`, `alamat_customer`, `paket_state`, `paket_ongkir`, `paket_driver`) VALUES
 (1, 1, 'daffa ganteng', '2022-05-20', '2022-05-21', 'rumah nugget', 'DELIVERING', 15000, 'Agus Joko Susilo'),
 (2, 23, 'daffa firmansyah', '2022-05-20', '0000-00-00', 'Malang', 'ON HOLD', 15000, 'Edwin Nurdiansyah'),
-(5, 34, 'daffa ganteng', '2022-05-21', '2022-05-21', 'rumah nugget', 'DELIVERING', 15000, 'Samuel Anderson');
+(5, 34, 'daffa ganteng', '2022-05-21', '2022-05-21', 'rumah nugget', 'DELIVERING', 15000, 'Samuel Anderson'),
+(6, 35, 'adrian natabuwana', '2022-06-12', '2022-06-12', 'Jakarta', 'ON HOLD', 15000, 'Freddy Yohanes Patty'),
+(7, 36, 'adrian natabuwana', '2022-06-12', '2022-06-12', 'Jakarta', 'DELIVERING', 15000, 'Ahmad Naufal');
 
 -- --------------------------------------------------------
 
@@ -95,7 +75,8 @@ INSERT INTO `pelanggan` (`pelanggan_id`, `nama`, `username`, `password`, `alamat
 (1, '', 'admin', '$2y$10$wj5NvJuauJfuBhTzB.pdg./VODNhzD.WpJdOcfkx5wGJr5K1vGkRm', '', '', 'admin@sisckae.com', 1),
 (2, 'daffa firmansyah', 'upilkeren', '$2y$10$KtkwoiCDH3Lh3Rs2Oc1AsuaDTBGh8Hq3rWFMVDmyGFRfMLKzU1NGu', 'Malang', '14045', 'upilkeren48@gmail.com', 0),
 (3, 'daffa ganteng', 'upilkeren28', '$2y$10$DLkwC./Akyxu79m3eFChreCcDPIME8KyVd6ubUR16WqHSEJ2E8zNK', 'rumah nugget', '081331857449', 'upilkeren28@gmail.com', 0),
-(5, 'bomby', 'bombom', '$2y$10$pghNycnPiN7vvcwLDKNEBuinQSvO56w7IdQ7NYgU.DXJOxLVYgMVW', 'malang', '085656690064', 'bombom@google.com', 0);
+(5, 'bomby', 'bombom', '$2y$10$pghNycnPiN7vvcwLDKNEBuinQSvO56w7IdQ7NYgU.DXJOxLVYgMVW', 'malang', '085656690064', 'bombom@google.com', 0),
+(6, 'adrian natabuwana', 'adrian', '$2y$10$kuL39pW/K7uiv8pC.cGU.OidtpvZbihoABXaQC.zAteA.ELNeo.2q', 'Jakarta', '08198765432', 'adrian@google.com', 0);
 
 -- --------------------------------------------------------
 
@@ -120,7 +101,9 @@ INSERT INTO `status_laundry` (`status_id`, `transaksi_id`, `status`) VALUES
 (55, 24, 'ON PROCESS'),
 (58, 29, 'ON HOLD'),
 (59, 30, 'FINISHED'),
-(63, 34, 'FINISHED');
+(63, 34, 'FINISHED'),
+(64, 35, 'ON HOLD'),
+(65, 36, 'ON HOLD');
 
 -- --------------------------------------------------------
 
@@ -150,17 +133,13 @@ INSERT INTO `transaksi` (`transaksi_id`, `nama_customer`, `tanggal_pemesanan`, `
 (24, 'daffa firmansyah', '2022-05-20', 'Bed Cover', 'Express', 'Pickup', 6, 63000),
 (29, 'daffa firmansyah', '2022-05-21', 'Bed Cover', 'Express', 'Pickup', 9, 78000),
 (30, 'daffa firmansyah', '2022-05-21', 'Selimut', 'Express', 'Pickup', 7, 71000),
-(34, 'daffa ganteng', '2022-05-21', 'Selimut', 'Express', 'Antar', 7, 86000);
+(34, 'daffa ganteng', '2022-05-21', 'Selimut', 'Express', 'Antar', 7, 86000),
+(35, 'adrian natabuwana', '2022-06-12', 'Pakaian', 'Express', 'Antar', 6, 75000),
+(36, 'adrian natabuwana', '2022-06-12', 'Bed Cover', 'Express', 'Antar', 4, 68000);
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `driver`
---
-ALTER TABLE `driver`
-  ADD PRIMARY KEY (`driver_id`);
 
 --
 -- Indexes for table `paket`
@@ -191,34 +170,28 @@ ALTER TABLE `transaksi`
 --
 
 --
--- AUTO_INCREMENT for table `driver`
---
-ALTER TABLE `driver`
-  MODIFY `driver_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
-
---
 -- AUTO_INCREMENT for table `paket`
 --
 ALTER TABLE `paket`
-  MODIFY `paket_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `paket_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `pelanggan`
 --
 ALTER TABLE `pelanggan`
-  MODIFY `pelanggan_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `pelanggan_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `status_laundry`
 --
 ALTER TABLE `status_laundry`
-  MODIFY `status_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+  MODIFY `status_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
 
 --
 -- AUTO_INCREMENT for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  MODIFY `transaksi_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `transaksi_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
